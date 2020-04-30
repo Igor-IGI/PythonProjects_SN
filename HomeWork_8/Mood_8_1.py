a20537829806a6f57069e4e3304469d18f8619ea
Igor Popovic <igor.tkd9@gmail.com>
2020-04-30 10:01:48 +0200

    Add Mood_8_1.py

---
+++ HomeWork_8/Mood_8_1.py
@@ -0,0 +1,14 @@
+mood = input("Enter your mode: ")
+
+if mood == "happy":
+    print("It is grate to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Go to do, what make you happy")
+elif mood == "excited":
+    print("It is nice to be excited")
+elif mood == "relaxed":
+    print("Chill and enjoy")
+else:
+    print("I don't recognize this mode")
